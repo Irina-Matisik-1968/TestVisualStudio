b1ef6589f30acf74e05226fbd97ce27ebeea44b3
Irina-Matisik <irina_matisik@mail.ru>
2022-12-04 22:34:16 +0300

    Добавьте файлы проекта.

---
+++ TestGit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, res;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            res = a + b;
+            Console.WriteLine($"{a}+{b}={res}");
+            res = a - b;
+            Console.WriteLine($"{a}-{b}={res}");
+            Console.ReadKey();
+        }
+    }
+}
